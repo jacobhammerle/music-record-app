64236f1670732f60db7019eeae48b80086231de4
Jacob Hammerle <hammerja@miamioh.edu>
2018-02-09 14:10:29 -0500

    project installation

--- tech_stack/index.android.js
+++
@@ -1,8 +0,0 @@
-import { AppRegistry, UIManager } from 'react-native';
-import App from './src/app';
-
-//Setting UIManager Flag in order to get LayoutAnimation work on android.
-UIManager.setLayoutAnimationEnabledExperimental && 
-UIManager.setLayoutAnimationEnabledExperimental(true);
-
-AppRegistry.registerComponent('tech_stack', () => App);
