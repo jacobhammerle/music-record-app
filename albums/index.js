5411badfaecd81c29e1ca39d025359d8a49d5545
Jacob Hammerle <hammerja@miamioh.edu>
2018-02-10 11:47:10 -0500

    Dynamic header component

--- albums/index.js
+++ albums/index.js
@@ -1,10 +1,11 @@
 // import a library to help create a component
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
+import Header from './src/components/header';
 
 // creat a component
 const App = () => (
-	<Text>Some Text</Text>
+	<Header headerText={'Albums'} />
 )
 
 // render it to the device
