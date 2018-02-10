cb9f1f21eed74f765c9c24f096f08a38c2089eb4
Jacob Hammerle <hammerja@miamioh.edu>
2018-02-10 13:56:56 -0500

    Album List Component and added react-devtools package

--- albums/index.js
+++ albums/index.js
@@ -1,11 +1,15 @@
 // import a library to help create a component
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry, View } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/AlbumList';
 
 // creat a component
 const App = () => (
-	<Header headerText={'Albums'} />
+	<View>
+		<Header headerText={'Albums'} />
+		<AlbumList />
+	</View>
 )
 
 // render it to the device
